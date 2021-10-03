d327beb9c0d0a5f77ca5803c31c42bba39a45771
CherryRedDragon <cherryreddragon@gmail.com>
2021-10-03 14:06:37 +0100

    first draft

---
+++ paintCalculator.py
@@ -0,0 +1,11 @@
+# algorithm for calculating the number of tins of paint needed for painting a wall with 2 coats of paint
+#M = meters
+#MSq = meters square
+wallHeightM = 5
+wallWidthM = 2
+wallAreaMSq = wallHeightM*wallWidthM
+#print(wallAreaMetersSquare)
+totalPaintAreaMSq = wallAreaMSq*2
+totalPaintAreaMSq/10
+
+
