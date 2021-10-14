22fb07600a2d1d23237d425850d23c180fdf05a8
CherryRedDragon <cherryreddragon@gmail.com>
2021-10-14 18:10:48 +0100

    added lists to ensure options are saved

--- dinnerChoices.py
+++ dinnerChoices.py
@@ -4,62 +4,68 @@ guestNumber = 1
 totalMain = 0
 totalDessert = 0
 
-#def guestChoice (guest1, guest2, guest3):
+guestList = []
+guestChoiceMain = []
+guestChoiceDessert = []
 
-
-while (guestNumber < 4):
-
-    #welcome guest and get name
-    print("Guest number", guestNumber)
-    guestName = input("Please type your name:")
-    print("Welcome "+ guestName +"!")
-
+def guestOrder():
     #ask guest if they want "main", "dessert", "both", or "none"
     main = str.lower(input("Would you like a main course? Please type 'Yes' if you do and 'No' if you do not: "))
     dessert = str.lower(input("Would you like a dessert course? Please type 'Yes' if you do and 'No' if you do not: "))
 
+    #add main
     if(main == "yes"):
         wantsMain = True
-        
-    if(dessert == "yes"):
-        wantsDessert = True
-        
-
-    if (wantsMain == True and wantsDessert == True):
-        print(guestName, "wants both main and dessert")
-        totalMain = totalMain + 1
-        totalDessert = totalDessert + 1
-
-    elif (wantsMain == False and wantsDessert == False):
-        print(guestName, "doesnt want any food")
-
-    elif (wantsMain == True and wantsDessert == False):
-        print(guestName, "only wants a main course")
+        guestChoiceMain.append("a main course")
+        global totalMain 
         totalMain = totalMain + 1
 
-    elif (wantsMain == False and wantsDessert == True):
-        print(guestName, "only wants a dessert")
-        totalDessert = totalDessert + 1
+    elif(main == "no"):
+        wantsMain = False
+        guestChoiceMain.append("no main course")
 
     else:
         print("Sorry, I dont think that's an option")
-
     
+    #add dessert    
+    if(dessert == "yes"):
+        wantsDessert = True
+        guestChoiceDessert.append("a dessert")
+        global totalDessert 
+        totalDessert = totalDessert + 1
+
+    elif(dessert == "no"):
+        wantsDessert = False
+        guestChoiceDessert.append("no dessert")
+
+    else:
+        print("Sorry, I dont think that's an option")   
+    return
+
+while (guestNumber < 4):
+
+    #welcome guest and get nameChoice
+    print("Guest number", guestNumber)
+    nameChoice = input("Please type your name:")
+    print("Welcome "+ nameChoice +"!")
+    #call guestOrder function to ask questions and update totals
+    guestOrder()
+    guestList.append(nameChoice)
     guestNumber = guestNumber + 1
-    wantsMain = False
-    wantsDessert = False
-    #print(guestNumber)
-
-
-
+    
+  
 #take guest choice and output total mains
 #output total desserts
-print("total number of main courses needed =", totalMain)
+print("\ntotal number of main courses needed =", totalMain)
 print("total number of desserts needed =", totalDessert)
+#output each guest's request
 
+# for guests in guestList:
+#     print(guests)
+# for mains in guestChoiceMain:
+#     print(mains)
+# for desserts in guestChoiceDessert:
+#     print(desserts)   
 
-#def order():
-    #return guestName, wantsMain, wantsDessert
-
-#print(wantsMain)
-#print(order)
+for x in range (3):
+    print(guestList[x], "would like", guestChoiceMain[x], "and", guestChoiceDessert[x] + ".")
